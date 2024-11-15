e6a41429a662c41b34d9b1f22a4da25aac3dafbd
Tarae0419 <tarae0419@gmail.com>
2024-11-16 01:33:10 +0900

    수정

--- Scripts/LoginManager.cs
+++ Scripts/LoginManager.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public partial class LoginManager : Node
 {
@@ -8,6 +10,23 @@ public partial class LoginManager : Node
 	private Label _IDLabel;
 	private Label _PasswordLabel;
 	private Button _LoginButton;
+	private Button _SignUpButton;
+	private Panel _SignInPanel;
+	private Panel _SignUpPanel;
+	
+	private String _startUI = "res://UI/StartUI.tscn";
+	
+	private static readonly Regex EmailRegex = new Regex(
+		@"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+		RegexOptions.Compiled
+	);
+
+	private Dictionary<string, string> _userDatabase = new Dictionary<string, string>
+    {
+        { "user1", "password1" },
+        { "user2", "password2" },
+        { "admin", "adminpass" }
+    };
 	
 	public override void _Ready()
 	{
@@ -16,17 +35,49 @@ public partial class LoginManager : Node
 		_IDLabel = GetNode<Label>("SignInPanel/IDLabel");
 		_PasswordLabel = GetNode<Label>("SignInPanel/PWLabel");
 		_LoginButton = GetNode<Button>("SignInPanel/LoginButton");
+		_SignUpButton = GetNode<Button>("SignInPanel/SignUpButton");
+		_SignInPanel = GetNode<Panel>("SignInPanel");
+		_SignUpPanel = GetNode<Panel>("SignUpPanel");
 		
 		_LoginButton.Connect("pressed",Callable.From(Login));
+		_SignUpButton.Connect("pressed", Callable.From(ShowSignUpPanel));
 
-	}
-	
-	public override void _Process(double delta)
-	{
 	}
 
 	private void Login()
 	{
-		
+		string enteredUsername = _IDEdit.Text;
+		string enteredPassword = _PasswordEdit.Text;
+
+		if (CheckLogin(enteredUsername, enteredPassword))
+		{
+			_SignInPanel.Hide();
+		}
+	}
+
+	private bool CheckLogin(string username, string password)
+	{
+		if (_userDatabase.ContainsKey(username) && _userDatabase[username] == password)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	private void ShowSignUpPanel()
+	{
+		_SignInPanel.Hide();
+		_SignUpPanel.Show();
+	}
+	
+	private void ShowSignInPanel()
+	{
+		_SignUpPanel.Hide();
+		_SignInPanel.Show();
+	}
+	
+	private bool IsEmailValid(string email)
+	{
+		return EmailRegex.IsMatch(email);
 	}
 }
