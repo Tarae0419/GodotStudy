0f80d164cbcda647643939675d4faf4560499b8d
Tarae0419 <tarae0419@gmail.com>
2024-11-16 06:01:14 +0900

    로그인 시스템 구현

--- Scripts/LoginManager.cs
+++ Scripts/LoginManager.cs
@@ -7,10 +7,13 @@ public partial class LoginManager : Node
 {
 	private LineEdit _IDEdit;
 	private LineEdit _PasswordEdit;
-	private Label _IDLabel;
-	private Label _PasswordLabel;
+	private LineEdit _CreateIDEdit;
+	private LineEdit _CreatePasswordEdit;
+	private LineEdit _CreatePasswordEdit2;
 	private Button _LoginButton;
+	private Button _SignInButton;
 	private Button _SignUpButton;
+	private Button _CreateButton;
 	private Panel _SignInPanel;
 	private Panel _SignUpPanel;
 	
@@ -23,24 +26,31 @@ public partial class LoginManager : Node
 
 	private Dictionary<string, string> _userDatabase = new Dictionary<string, string>
     {
-        { "user1", "password1" },
-        { "user2", "password2" },
-        { "admin", "adminpass" }
+        { "a@naver.com", "a" },
+        { "b@naver.com", "b" },
+        { "c@naver.com", "c" }
     };
 	
 	public override void _Ready()
 	{
 		_IDEdit = GetNode<LineEdit>("SignInPanel/IDEdit");
 		_PasswordEdit = GetNode<LineEdit>("SignInPanel/PWEdit");
-		_IDLabel = GetNode<Label>("SignInPanel/IDLabel");
-		_PasswordLabel = GetNode<Label>("SignInPanel/PWLabel");
+		_CreateIDEdit = GetNode<LineEdit>("SignUpPanel/CreateIDEdit");
+		_CreatePasswordEdit = GetNode<LineEdit>("SignUpPanel/CreatePWEdit");
+		_CreatePasswordEdit2 = GetNode<LineEdit>("SignUpPanel/CreatePWEdit2");
+		
 		_LoginButton = GetNode<Button>("SignInPanel/LoginButton");
+		_SignInButton = GetNode<Button>("SignUpPanel/SignInButton");
 		_SignUpButton = GetNode<Button>("SignInPanel/SignUpButton");
+		_CreateButton = GetNode<Button>("SignUpPanel/CreateButton");
+		
 		_SignInPanel = GetNode<Panel>("SignInPanel");
 		_SignUpPanel = GetNode<Panel>("SignUpPanel");
 		
 		_LoginButton.Connect("pressed",Callable.From(Login));
 		_SignUpButton.Connect("pressed", Callable.From(ShowSignUpPanel));
+		_SignInButton.Connect("pressed", Callable.From(ShowSignInPanel));
+		_CreateButton.Connect("pressed", Callable.From(CreateUser));
 
 	}
 
@@ -49,9 +59,17 @@ public partial class LoginManager : Node
 		string enteredUsername = _IDEdit.Text;
 		string enteredPassword = _PasswordEdit.Text;
 
-		if (CheckLogin(enteredUsername, enteredPassword))
+		if (CheckLogin(enteredUsername, enteredPassword) && IsEmailValid(enteredUsername))
 		{
 			_SignInPanel.Hide();
+		}
+		else if (!IsEmailValid(enteredUsername))
+		{
+			GD.Print("이메일 형식이 아닙니다");
+		}
+		else
+		{
+			GD.Print("아이디 또는 비밀번호을 확인해 주세요");
 		}
 	}
 
@@ -79,5 +97,46 @@ public partial class LoginManager : Node
 	private bool IsEmailValid(string email)
 	{
 		return EmailRegex.IsMatch(email);
+	}
+
+	private void CreateUser()
+	{
+		string enteredUsername = _CreateIDEdit.Text;
+		string enteredPassword = _CreatePasswordEdit.Text;
+		string enteredConfirmPassword = _CreatePasswordEdit2.Text;
+
+		if (!IsEmailValid(enteredUsername) && !ConfirmPassword(enteredPassword, enteredConfirmPassword))
+		{
+			GD.Print("이메일과 비밀번호를 입력해주세요.");
+		}
+		else if (!IsEmailValid(enteredUsername))
+		{
+			GD.Print("이메일 형식이 옳바르지 않습니다.");
+		}
+		else if(!ConfirmPassword(enteredPassword, enteredConfirmPassword))
+		{
+			GD.Print("입력한 비밀번호가 서로 다릅니다.");
+		}
+		else
+		{
+			GD.Print("이메일과 비밀번호를 입력해주세요.");
+			GD.Print("생성 완료!");
+			_userDatabase.Add(enteredUsername, _CreatePasswordEdit.Text);
+			ShowSignInPanel();
+		}
+	}
+
+	private bool ConfirmPassword(string enteredPassword, string enteredConfirmPassword)
+	{
+		if (enteredPassword == "" && enteredConfirmPassword == "")
+		{
+			return false;
+		}
+		else if (enteredPassword.Equals(enteredConfirmPassword))
+		{
+			return true;
+		}
+
+		return false;
 	}
 }
